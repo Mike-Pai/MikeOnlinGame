a0f668b92fa30aa522e2324cbc5cf75d902e6ac9
MikePai <ye518612@gmail.com>
2021-04-14 16:09:16 +0800

    test commit

--- MikeOnlinGame/MikeOnlinGameApp.swift
+++ MikeOnlinGame/MikeOnlinGameApp.swift
@@ -11,6 +11,7 @@ import Firebase
 @main
 struct MikeOnlinGameApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
