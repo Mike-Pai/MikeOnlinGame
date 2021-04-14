a58c3118f0adba1c26c5a82617a1b5bb2276ce28
MikePai <ye518612@gmail.com>
2021-04-14 15:30:11 +0800

    Initial Commit

---
+++ MikeOnlinGame/MikeOnlinGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikeOnlinGameApp.swift
+//  MikeOnlinGame
+//
+//  Created by 白謹瑜 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct MikeOnlinGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
